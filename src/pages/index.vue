b262a86fc9156d24831a36368891dd73ab06e5f8
Tina <tina.chen@u-ark.com>
2024-07-02 16:39:36 +0800

    doc: add nav & banner

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,54 +1,57 @@
 <template>
-  <Layout title="過勞量表">
-    <template #content>
-      <div class="flex flex-col gap-6 md:gap-10">
-        <!-- 說明 -->
-        <div class="p-4 tracking-wider text-gray-500 bg-gray-200 md:px-8 rounded-xl">
-          <h2>
-            親愛的同仁您好:
-            <br/>
-            此「過勞量表」是勞動部針對過勞壓力反應編製；可協助在短時間內瞭解工作壓力引起的過勞反應或過勞傾向。對於您所提供的資料，將只用於公司醫務室健康管理分析使用，而且絕對保密，請安心填寫，謝謝!
-          </h2>
-        </div>
-  
-        <!-- 個人疲勞 -->
-        <div>
-          <h3 class="pl-4 mb-4 text-lg border-l-8 border-black md:text-2xl">個人疲勞</h3>
-          <div class="flex flex-col gap-6">
-            <div v-for="(person, index) in person" :key="index" >
-              <p class="mb-2 md:text-lg">
-                {{ person.id }}.{{ person.title }}
-              </p>
-              <RadioButtonGroup  :options="['總是', '常常', '有時候', '不常', '從未或幾乎從未']" class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-4 md:gap-x-12 h-fit" />
+  <div class="relative">
+    <Banner />
+    <Layout title="過勞量表">
+      <template #content>
+        <div class="flex flex-col gap-6 md:gap-10">
+          <!-- 說明 -->
+          <div class="p-4 tracking-wider text-gray-500 bg-gray-200 md:px-8 rounded-xl">
+            <h2>
+              親愛的同仁您好:
+              <br/>
+              此「過勞量表」是勞動部針對過勞壓力反應編製；可協助在短時間內瞭解工作壓力引起的過勞反應或過勞傾向。對於您所提供的資料，將只用於公司醫務室健康管理分析使用，而且絕對保密，請安心填寫，謝謝!
+            </h2>
+          </div>
+    
+          <!-- 個人疲勞 -->
+          <div>
+            <h3 class="pl-4 mb-4 text-lg border-l-8 border-black md:text-2xl">個人疲勞</h3>
+            <div class="flex flex-col gap-6">
+              <div v-for="(person, index) in person" :key="index" >
+                <p class="mb-2 md:text-lg">
+                  {{ person.id }}.{{ person.title }}
+                </p>
+                <RadioButtonGroup  :options="['總是', '常常', '有時候', '不常', '從未或幾乎從未']" class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-4 md:gap-x-12 h-fit" />
+              </div>
             </div>
           </div>
-        </div>
 
-        <!-- 工作疲勞 -->
-        <div>
-          <h3 class="pl-4 mb-4 text-lg border-l-8 border-black md:text-2xl">工作疲勞</h3>
-          <div class="flex flex-col gap-6">
-            <div v-for="(work, index) in work" :key="index" >
-              <p class="mb-2 md:text-lg">
-                {{ work.id }}.{{ work.title }}
-              </p>
-              <RadioButtonGroup :options="['總是', '常常', '有時候', '不常', '從未或幾乎從未']"  class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-4 md:gap-x-12 h-fit" />
+          <!-- 工作疲勞 -->
+          <div>
+            <h3 class="pl-4 mb-4 text-lg border-l-8 border-black md:text-2xl">工作疲勞</h3>
+            <div class="flex flex-col gap-6">
+              <div v-for="(work, index) in work" :key="index" >
+                <p class="mb-2 md:text-lg">
+                  {{ work.id }}.{{ work.title }}
+                </p>
+                <RadioButtonGroup :options="['總是', '常常', '有時候', '不常', '從未或幾乎從未']"  class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-4 md:gap-x-12 h-fit" />
+              </div>
             </div>
           </div>
-        </div>
 
-        
-        <!-- 確認 -->
-        <Confirm />
+          
+          <!-- 確認 -->
+          <Confirm />
 
-        <!-- 按鈕 -->
-        <div class="flex items-center justify-center gap-4">
-          <SecondaryButton label="取消" />
-          <PrimaryButton label="送出" />
+          <!-- 按鈕 -->
+          <div class="flex items-center justify-center gap-4">
+            <SecondaryButton label="取消" />
+            <PrimaryButton label="送出" />
+          </div>
         </div>
-      </div>
-    </template>
-  </Layout>
+      </template>
+    </Layout>
+  </div>
 </template>
 
 <script>
