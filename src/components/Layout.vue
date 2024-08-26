fcc50fd34d10d7db9a329776923e757869b17ad8
Tina <tina.chen@u-ark.com>
2024-08-26 14:51:20 +0800

    feat: typewriter

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -3,8 +3,8 @@
   
     <!-- 內容區塊 -->
     <section class="h-full">
-      <div class="container max-w-5xl px-4 py-6 mx-auto sm:px-6 lg:px-8 lg:py-10">
-        <h1 class="mb-8 text-xl text-center md:mb-10 md:text-3xl">{{ title }}</h1>
+      <div class="px-4 py-6 mx-auto sm:px-6 xl:px-28 xl:pt-40">
+        <!-- <h1 class="mb-8 text-xl text-center md:mb-10 md:text-3xl">{{ title }}</h1> -->
         <slot name="content"></slot>
       </div>
     </section>
