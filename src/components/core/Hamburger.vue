acb1bc6845a7383192d464d479453617b8ccb060
Tina <tina.chen@u-ark.com>
2024-08-26 11:03:38 +0800

    feat: navbar

---
+++ src/components/core/Hamburger.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="px-4 py-3 border-2 border-white rounded-full xl:hidden">
+        <div @click="toggleMenu" :class="{'open': isOpen}" class="hamburger">
+      <span></span>
+      <span></span>
+    </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      isOpen: {
+        type: Boolean,
+        required: true
+      },
+      toggleMenu: {
+        type: Function,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .hamburger {
+    width: 24px; /* Width of the hamburger button */
+    height: 16px; /* Height of the hamburger button */
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
+    position: relative;
+  }
+  
+  .hamburger span {
+    display: block;
+    height: 3px; /* Height of each bar */
+    background-color: #fff;
+    transition: transform 0.3s ease, width 0.3s ease, top 0.3s ease;
+    position: absolute;
+    left: 0;
+    width: 100%; /* Default width is 100% */
+  }
+  
+  .hamburger span:first-child {
+    top: 0; /* Position of the first bar */
+  }
+  
+  .hamburger span:last-child {
+    top: 17px; /* Position of the last bar */
+    width: 70%; /* Default width for the last bar */
+  }
+  
+  .hamburger.open span:first-child {
+    transform: rotate(45deg);
+    top: 50%;
+    transform-origin: center;
+    width: 100%; /* Ensure the cross bar is as wide as the hamburger */
+  }
+  
+  .hamburger.open span:last-child {
+    transform: rotate(-45deg);
+    top: 50%;
+    transform-origin: center;
+    width: 100%; /* Ensure the cross bar is as wide as the hamburger */
+  }
+  </style>
+  
--- src/components/core/Navbar.vue
+++ src/components/core/Navbar.vue
@@ -1,88 +1,104 @@
 <template>
-  <div class="sticky z-20 pt-6 px-28 ">
-    <div class="bg-blue-100 rounded-full shadow-md bg-blur-lg">
-      <div class="container relative flex flex-col items-center justify-between w-full px-4 py-3.5 mx-auto text-white sm:flex-row sm:px-6">
-          <!-- Logo -->
-          <div class="flex items-center justify-between w-full ">
-            <div class="inline-flex items-center space-x-3.5 group">
-              <RouterLink to="/" class="inline-flex items-center justify-center transition-colors duration-300 rounded-full h-11 w-11 bg-stone-900 group-hover:bg-Secondary">
-                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="22" fill="none" viewBox="0 0 23 22" class="transition-colors duration-300 stroke-white group-hover:stroke-stone-900">
-                  <path stroke-width="2" d="M1 4.778s2.907-2.573 6.51-2.573c6.076 0 11.343 2.4 13.279 1.432s.538-3.334-1.396-2.438c-3.645 1.689-4.688 7.173-7.2 12.198-2.513 5.026-6.168 7.31-8.452 6.853-1.161-.232-1.719-1.413-1.028-2.284 1.63-2.055 4.468 1.504 8.094 2.76 1.783.617 3.566 0 3.566 0"/>
-                </svg>
-              </RouterLink>
-              <h1 class="text-xl font-medium cursor-default">
-                Tina Chen
-              </h1>
-            </div>
-
-
-    
-            <!-- 手機選單按鈕 -->
-            <button type="button" 
-            class="w-8 h-8 ml-auto sm:hidden"
-            @click="toggleMenu">
-              <heroicons-outline-menu class="w-7 h-7" />
-            </button>
+  <div class="sticky z-20 pt-6 px-28">
+    <div class="bg-blue-100 shadow-md rounded-3xl xl:rounded-full bg-blur-lg">
+      <div class="container relative flex flex-row items-center justify-between w-full p-5 py-3.5 mx-auto text-white sm:flex-row sm:px-6 ">
+        <!-- Logo -->
+        <div class="flex items-center justify-between w-full">
+          <div class="inline-flex items-center space-x-3.5 group">
+            <RouterLink to="/" class="inline-flex items-center justify-center transition-colors duration-300 rounded-full h-11 w-11 bg-stone-900 group-hover:bg-Secondary">
+              <!-- Your SVG Logo -->
+              <svg xmlns="http://www.w3.org/2000/svg" width="23" height="22" fill="none" viewBox="0 0 23 22" class="transition-colors duration-300 stroke-white group-hover:stroke-stone-900">
+                <path stroke-width="2" d="M1 4.778s2.907-2.573 6.51-2.573c6.076 0 11.343 2.4 13.279 1.432s.538-3.334-1.396-2.438c-3.645 1.689-4.688 7.173-7.2 12.198-2.513 5.026-6.168 7.31-8.452 6.853-1.161-.232-1.719-1.413-1.028-2.284 1.63-2.055 4.468 1.504 8.094 2.76 1.783.617 3.566 0 3.566 0"/>
+              </svg>
+            </RouterLink>
+            <h1 class="text-xl font-medium cursor-default">
+              Tina Chen
+            </h1>
           </div>
-    
-          <div class="flex items-center justify-center gap-10">
-            <!-- 主要選單 -->
-            <div 
-            class="sm:flex sm:flex-col sm:justify-between whitespace-nowrap"
-            :class="showMenu ? '' : 'hidden'">
-              <ul class="flex flex-col text-sm font-semibold sm:flex-row">
-                <li v-for="item in menuItems" :key="item.to">
-                  <RouterLink :to="item.to" 
-                  class="flex items-center px-4 py-2.5 transition-colors duration-200 sm:px-5 whitespace-nowrap"
-                  :class="isActive(item.to) ? 'text-black' : 'text-blue-500  hover:text-white'">
+
+        </div>
+
+        <div class="flex items-center justify-center gap-10">
+          <!-- Web Menu -->
+          <div class="hidden xl:block">
+            <div class="sm:flex sm:flex-row sm:justify-between whitespace-nowrap">
+            <ul class="flex flex-col text-sm font-semibold sm:flex-row">
+              <li v-for="item in menuItems" :key="item.to">
+                <RouterLink :to="item.to" 
+                class="flex items-center px-4 py-2.5 transition-colors duration-200 sm:px-5 whitespace-nowrap"
+                :class="isActive(item.to) ? 'text-black' : 'text-blue-500  hover:text-white'">
                   {{ $t(item.text) }}
-                  </RouterLink>
-                </li>
-                <li>
-                  <button type="button" class="px-4 py-2.5">
-                    下載履歷
-                  </button>
-                </li>
-              </ul>
-            </div>
+                </RouterLink>
+              </li>
+              <li>
+                <button type="button" class="px-4 py-2.5">
+                  下載履歷
+                </button>
+              </li>
+            </ul>
             <LanguageSwitcher />
           </div>
+          </div>
+          
+        </div>
+        <!-- Pass the toggleMenu method and isOpen state -->
+        <Hamburger :toggleMenu="toggleMenu" :isOpen="showMenu" />
+      </div>
+      <!-- Mobile Menu -->
+      <div class="xl:hidden">
+        <div v-if="showMenu" class="fixed inset-0 bg-black bg-opacity-10 -z-10 xl:hidden backdrop-blur-lg" @click="toggleMenu"></div>
+        <div v-if="showMenu" class="z-20 flex flex-col justify-between p-5 whitespace-nowrap">
+          <ul class="flex flex-col text-sm font-semibold ">
+            <li v-for="item in menuItems" :key="item.to">
+              <RouterLink :to="item.to" 
+              class="flex items-center px-4 py-2.5 transition-colors duration-200  whitespace-nowrap"
+              :class="isActive(item.to) ? 'text-black' : 'text-blue-500  hover:text-white'">
+                {{ $t(item.text) }}
+              </RouterLink>
+            </li>
+            <li>
+              <button type="button" class="px-4 py-2.5">
+                下載履歷
+              </button>
+              <LanguageSwitcher />
+            </li>
+          </ul>
+          
+        </div>
       </div>
     </div>
   </div>
 </template>
-  
-  <script>
-  import { computed, ref } from "vue";
-  import { useRoute, useRouter } from 'vue-router';
-  
-  export default {
-    setup() {
-      const route = useRoute()
-      const router = useRouter()
-  
-      const showMenu = ref(false)
-      const toggleMenu = () => showMenu.value = !showMenu.value
-  
-      // 「主選單按鈕」功能
-      const menuItems = [
-        { to: '/', text: 'menu.item1' },
-        { to: '/list_1', text: 'menu.item2' },
-        { to: '/list_2', text: 'menu.item3' },
-        { to: '/list_2', text: 'menu.item4' }
-      ]
 
-      const activeItem = computed(() => 
-        [...menuItems]
-          .reverse()
-          .find(item => route.path.startsWith(item.to))
-      )
-  
-      const isActive = to => to === activeItem.value.to
-  
-      return { showMenu, toggleMenu, menuItems, isActive }
-    }
+<script>
+import { computed, ref } from "vue";
+import { useRoute, useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+
+    const showMenu = ref(false);
+    const toggleMenu = () => showMenu.value = !showMenu.value;
+
+    // Main menu items
+    const menuItems = [
+      { to: '/', text: 'menu.item1' },
+      { to: '/list_1', text: 'menu.item2' },
+      { to: '/list_2', text: 'menu.item3' },
+      { to: '/list_3', text: 'menu.item4' }
+    ];
+
+    const activeItem = computed(() => 
+      [...menuItems]
+        .reverse()
+        .find(item => route.path.startsWith(item.to))
+    );
+
+    const isActive = to => to === activeItem.value.to;
+
+    return { showMenu, toggleMenu, menuItems, isActive };
   }
-  </script>
-  
-  
+}
+</script>
